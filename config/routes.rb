4697fa6b1a7b0841eea52eb3d886685aa6e7926c
David Davydov <kctrlv@gmail.com>
2016-10-25 00:28:06 -0600

    implement oauth handroll for slack

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  get 'auth/slack/callback', to: 'sessions#create'
+
+  get '/logout', to: 'sessions#destroy'
+
+  get '/restricted', to: 'home#restricted'
 end
