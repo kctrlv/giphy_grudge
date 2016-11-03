2c401f91a7a4acfb3da67b9aa4afff5dc3eaf031
David Davydov <kctrlv@gmail.com>
2016-11-03 10:08:49 -0600

    creating controller and views for games

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,9 @@ class SessionsController < ApplicationController
       user.token = raw_res[:access_token]
       user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to games_path
     else
-      flash[:danger] = "Sorry, this app is restricted to users of a particular Team"
+      flash[:danger] = "Signing in with Slack is only available for the Turing team"
       redirect_to '/'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :games, only: [:index]
   root to: 'home#index'
-
   get 'auth/slack/callback', to: 'sessions#create'
-
   get '/logout', to: 'sessions#destroy'
-
   get '/restricted', to: 'home#restricted'
 end
