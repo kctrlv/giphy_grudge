e703aa5fd94811a185bd37f6c94f9d07868b4bcf
David Davydov <kctrlv@gmail.com>
2016-11-03 09:51:40 -0600

    add 197 cards to seed db with

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025060912) do
+ActiveRecord::Schema.define(version: 20161103152207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.text "title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.text     "uid"
