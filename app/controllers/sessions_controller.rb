89a00e0072d1b38aae0d87ae47e0151b3c1e4853
David Davydov <kctrlv@gmail.com>
2016-11-03 16:22:06 -0600

    user can play as guest

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,40 @@
 class SessionsController < ApplicationController
   def create
-    byebug
-    res = Faraday.get("https://slack.com/api/oauth.access?client_id=#{ENV['slack_id']}&client_secret=#{ENV['slack_secret']}&code=#{params['code']}")
-    raw_res = JSON.parse(res.body, symbolize_names:true)
-    if raw_res[:team][:id] == ENV['turing_team_id']
-      user = User.find_or_create_by(uid: raw_res[:user][:id])
-      user.name = raw_res[:user][:name]
-      user.uid = raw_res[:user][:id]
-      user.token = raw_res[:access_token]
+    if params['code'] #Slack sign in - refactor out
+      res = Faraday.get("https://slack.com/api/oauth.access?client_id=#{ENV['slack_id']}&client_secret=#{ENV['slack_secret']}&code=#{params['code']}")
+      raw_res = JSON.parse(res.body, symbolize_names:true)
+      if raw_res[:team][:id] == ENV['turing_team_id']
+        user = User.find_or_create_by(uid: raw_res[:user][:id])
+        user.name = raw_res[:user][:name]
+        user.uid = raw_res[:user][:id]
+        user.token = raw_res[:access_token]
+        user.save
+        session[:user_id] = user.id
+        redirect_to lobby_path
+      else
+        flash[:danger] = "Sorry, this app is restricted to users of a particular Team"
+        redirect_to '/'
+      end
+    else # Guest login
+      uid = SecureRandom.hex[0..4]
+      user = User.new(uid: uid)
+      user.name = "Guest_#{uid}"
       user.save
       session[:user_id] = user.id
       redirect_to lobby_path
-    else
-      flash[:danger] = "Sorry, this app is restricted to users of a particular Team"
-      redirect_to '/'
     end
   end
 
   def destroy
+    unless current_user.token
+      current_user.destroy
+    end
     session[:user_id] = nil
     redirect_to '/'
   end
+
+  private
+    def session_params
+      params.require(:session).permit(:username, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'auth/slack/callback', to: 'sessions#create'
 
   get '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
 
   get '/restricted', to: 'home#restricted'
 end
