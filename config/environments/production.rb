5eeb9e981de8b45a779116729f1c248f2f802af1
David Davydov <kctrlv@gmail.com>
2016-11-03 19:27:17 -0600

    change https to http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://giphyproject.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://giphyproject.herokuapp.com/' ]
+  config.action_cable.allowed_request_origins = [ 'http://giphyproject.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
