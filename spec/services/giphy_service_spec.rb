e826f02a45e9046123324a38565839d61422eb6e
David Davydov <kctrlv@gmail.com>
2016-11-03 18:21:28 -0600

    giphyservice translate keyword to image

--- spec/services/giphy_service_spec.rb
+++ spec/services/giphy_service_spec.rb
@@ -1,9 +1,15 @@
 require "rails_helper"
 
 RSpec.describe GiphyService do
-  it 'can get fixed height image by keyword' do
+  it 'can get full translation by keyword' do
     keyword = 'superman'
     result = GiphyService.translate(keyword)
-    byebug
+    expect(result[:data][:type]).to eq("gif")
+  end
+
+  it 'can get fixed height image by keyword' do
+    keyword = 'superman'
+    result = GiphyService.fixed_height_translate(keyword)
+    expect(result[-7..-1]).to eq('200.gif')
   end
 end
