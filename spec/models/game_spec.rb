5bdc22bf67dddc720c990df8a4e47a8a0c543f55
David Davydov <kctrlv@gmail.com>
2016-11-03 18:57:26 -0600

    remove unneeded spec in game

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Game, type: :model do
   context "validations" do
     it { should validate_presence_of(:status)}
   end
-
-  context 'associations' do
-    it { should have_many(:users) }
-  end
 end
